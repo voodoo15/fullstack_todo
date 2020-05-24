3c11a718d74b52019cf072f357a48fd7d0638b06
Arnold Redoblado <VOODOO15@VOODOO15-MBP.lan>
2020-05-24 12:30:57 -0400

    Set up Apollo and GraphQL boilerplate

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 import express from 'express';
+const bodyParser = require('body-parser');
+const {
+    graphqlExpress,
+    graphiqlExpress
+} = require('apollo-server-express');
+const {
+    makeExecutableSchema
+} = require('graphql-tools');
 const server = express();
+
+server.use(
+    '/graphiql',
+    bodyParser.json(),
+    graphiqlExpress({
+        endpointURL: '/graphql',
+    }),
+);
+
+server.use(
+    '/graphql',
+    graphqlExpress({})
+);
 
 server.listen(4000, () => {
     console.log('listening on port 4000');
